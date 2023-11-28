8018a2de6b284396f4dad78de9600fe8bbb7471a
Adriel Estoller <adrielestoller@gmail.com>
2023-11-28 15:22:14 -0300

    Login page completa

--- lib/common/style_txt.dart
+++ lib/common/style_txt.dart
@@ -6,4 +6,25 @@ class AppStyle {
     fontWeight: FontWeight.w700,
     color: Colors.black,
   );
+
+  static const loginHeadingPurpleText = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: Colors.deepPurple,
+    decoration: TextDecoration.underline,
+    decorationThickness: 2,
+  );
+
+  static const titleText = TextStyle(
+    fontSize: 36,
+    fontWeight: FontWeight.w800,
+    color: Colors.deepPurple,
+    shadows: [
+      Shadow(
+        offset: Offset(1.5, 1.5),
+        color: Colors.black,
+        blurRadius: 1,
+      )
+    ],
+  );
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:gengarfilm/common/style_txt.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,11 +14,59 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
+              flex: 2,
               child: Image.asset(
                 'assets/gengar_splash.png',
                 scale: 3,
               ),
             ),
+            const Text(
+              'Gengar Films',
+              style: AppStyle.titleText,
+            ),
+            const Gap(10),
+            Container(
+              height: 60,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                border: Border.all(color: Colors.deepPurple),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                // gradient: Gradient(
+                //   Alignment.topCenter,
+                //   Alignment.bottomCenter,
+                //   [Colors.deepPurple, Colors.purple],
+                // ),
+              ),
+              child: const Center(
+                child: Text(
+                  "Iniciar sessão",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const Gap(10),
+            const Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Não tem conta?',
+                    style: AppStyle.headingText,
+                  ),
+                  Gap(5),
+                  Text(
+                    'Cadastre-se!',
+                    style: AppStyle.loginHeadingPurpleText,
+                  )
+                ],
+              ),
+            ),
+            const Gap(10),
           ],
         ),
       ),
