bc4c1e8ce6755cd4f663a10996466f5aa2688637
Adriel Estoller <adrielestoller@gmail.com>
2023-11-28 11:32:17 -0300

    Criação e configuração da Splash Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gengarfilm/pages/home_page.dart';
 
 void main() {
+  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(
     ProviderScope(
       child: MaterialApp(
@@ -16,4 +19,6 @@ void main() {
       ),
     ),
   );
+
+  // FlutterNativeSplash.remove();
 }
