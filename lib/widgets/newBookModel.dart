7810d822ff409ed9da34bd72b85356b1cf6c9224
Adriel Estoller <adrielestoller@gmail.com>
2023-11-28 08:25:10 -0300

    newBookModal consertado com scrollview + botão cancel

--- lib/widgets/newBookModel.dart
+++ lib/widgets/newBookModel.dart
@@ -15,158 +15,170 @@ class newBookModel extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      padding: const EdgeInsets.all(30),
-      height: MediaQuery.of(context).size.height * 0.9,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16), color: Colors.white),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: Text(
-              'Adicionar novo livro',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onVerticalDragEnd: (details) {
+        if (details.primaryVelocity! > 0) {
+          Navigator.pop(context); // Fecha o modal ao arrastar para baixo
+        }
+      },
+      child: SingleChildScrollView(
+        padding: EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Text(
+                'Adicionar novo livro',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Divider(
-            thickness: 1.2,
-            color: Colors.grey.shade200,
-          ),
-          const Gap(12),
-          const Text(
-            'Titulo do Livro',
-            style: appStyle.headingText,
-          ),
-          const Gap(6),
-          customTextField(
-            hintTxt: 'Escreva o titulo do seu livro',
-          ),
-          const Gap(12),
-          Text(
-            'Autor do livro',
-            style: appStyle.headingText,
-          ),
-          const Gap(6),
-          customTextField(
-            hintTxt: 'Escreva o nome do autor',
-          ),
-          const Gap(12),
-          Text(
-            'Descrição',
-            style: appStyle.headingText,
-          ),
-          const Gap(6),
-          customTextField(
-            hintTxt: "Descrição do livro",
-            maxLength: 255,
-            maxLines: 3,
-          ),
-          Gap(12),
-          Text(
-            'Status',
-            style: appStyle.headingText,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: radioWidget(
-                    radioTxt: 'Nada',
-                    statusColor: Colors.red.shade400,
-                    valueInput: 1,
-                    onChangedValue: () => {
-                          ref.read(radioProvider.notifier).update((state) => 1),
-                        }),
-              ),
-              Expanded(
-                child: radioWidget(
-                    radioTxt: 'Lendo',
-                    statusColor: Colors.purple.shade200,
-                    valueInput: 2,
-                    onChangedValue: () => {
-                          ref.read(radioProvider.notifier).update((state) => 2),
-                        }),
-              ),
-              Expanded(
-                child: radioWidget(
-                    radioTxt: 'Lido',
-                    statusColor: Colors.deepPurple,
-                    valueInput: 3,
-                    onChangedValue: () => {
-                          ref.read(radioProvider.notifier).update((state) => 3),
-                        }),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              dateTimeWidget(
-                icon: CupertinoIcons.calendar,
-                dateTimeText: 'Inicio da leitura',
-                valueText: 'dia/mês/ano',
-              ),
-              Gap(30),
-              dateTimeWidget(
-                dateTimeText: 'Horario de leitura',
-                valueText: ' 12:00',
-                icon: CupertinoIcons.time,
-              )
-            ],
-          ),
-          Gap(13),
-          Row(
-            children: [
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    side: BorderSide(color: Colors.deepPurple),
-                    backgroundColor: const Color.fromARGB(255, 239, 231, 240),
-                    foregroundColor: Colors.deepPurple,
-                    elevation: 10,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
+            Divider(
+              thickness: 1.2,
+              color: Colors.grey.shade200,
+            ),
+            const Gap(12),
+            const Text(
+              'Titulo do Livro',
+              style: appStyle.headingText,
+            ),
+            const Gap(6),
+            customTextField(
+              hintTxt: 'Escreva o titulo do seu livro',
+            ),
+            const Gap(12),
+            Text(
+              'Autor do livro',
+              style: appStyle.headingText,
+            ),
+            const Gap(6),
+            customTextField(
+              hintTxt: 'Escreva o nome do autor',
+            ),
+            const Gap(12),
+            Text(
+              'Descrição',
+              style: appStyle.headingText,
+            ),
+            const Gap(6),
+            customTextField(
+              hintTxt: "Descrição do livro",
+              maxLength: 255,
+              maxLines: 3,
+            ),
+            Gap(12),
+            Text(
+              'Status',
+              style: appStyle.headingText,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: radioWidget(
+                      radioTxt: 'Nada',
+                      statusColor: Colors.red.shade400,
+                      valueInput: 1,
+                      onChangedValue: () => {
+                            ref
+                                .read(radioProvider.notifier)
+                                .update((state) => 1),
+                          }),
+                ),
+                Expanded(
+                  child: radioWidget(
+                      radioTxt: 'Lendo',
+                      statusColor: Colors.purple.shade200,
+                      valueInput: 2,
+                      onChangedValue: () => {
+                            ref
+                                .read(radioProvider.notifier)
+                                .update((state) => 2),
+                          }),
+                ),
+                Expanded(
+                  child: radioWidget(
+                      radioTxt: 'Lido',
+                      statusColor: Colors.deepPurple,
+                      valueInput: 3,
+                      onChangedValue: () => {
+                            ref
+                                .read(radioProvider.notifier)
+                                .update((state) => 3),
+                          }),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                dateTimeWidget(
+                  icon: CupertinoIcons.calendar,
+                  dateTimeText: 'Inicio da leitura',
+                  valueText: 'dia/mês/ano',
+                ),
+                Gap(30),
+                dateTimeWidget(
+                  dateTimeText: 'Horario de leitura',
+                  valueText: ' 12:00',
+                  icon: CupertinoIcons.time,
+                )
+              ],
+            ),
+            Gap(13),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      side: BorderSide(color: Colors.deepPurple),
+                      backgroundColor: const Color.fromARGB(255, 239, 231, 240),
+                      foregroundColor: Colors.deepPurple,
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 14),
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'Cancelar',
-                    style: TextStyle(
-                        color: Colors.purple, fontWeight: FontWeight.bold),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      'Cancelar',
+                      style: TextStyle(
+                          color: Colors.purple, fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
-              ),
-              Gap(30),
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    side: BorderSide(color: Colors.deepPurple),
-                    backgroundColor: Colors.deepPurple,
-                    foregroundColor: Colors.white,
-                    elevation: 10,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
+                Gap(30),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      side: BorderSide(color: Colors.deepPurple),
+                      backgroundColor: Colors.deepPurple,
+                      foregroundColor: Colors.white,
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 14),
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'Concluir',
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+                    onPressed: () {},
+                    child: const Text(
+                      'Concluir',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
